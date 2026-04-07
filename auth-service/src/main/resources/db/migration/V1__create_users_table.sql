c3804807051aa428ce1bb1c59d4caceea2955616
D1ff1cult42 <orbitovkek@gmail.com>
2026-04-08 01:35:10 +0300

    :sparkles: :card_file_box: add flyway migration V1

---
+++ auth-service/src/main/resources/db/migration/V1__create_users_table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE users
+(
+    id            UUID         NOT NULL,
+    email         VARCHAR(255) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    verified      BOOLEAN      NOT NULL,
+    created_at    TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    updated_at    TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    role          VARCHAR(255),
+    CONSTRAINT pk_users PRIMARY KEY (id)
+);
+
+ALTER TABLE users
+    ADD CONSTRAINT uc_users_email UNIQUE (email);
