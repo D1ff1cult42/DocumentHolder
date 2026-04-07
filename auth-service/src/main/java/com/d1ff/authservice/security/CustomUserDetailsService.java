3a0bd5e446f59b1b4a4209bd610cfc96ddbb7f56
D1ff1cult42 <orbitovkek@gmail.com>
2026-04-08 01:20:49 +0300

    :lock: :sparkles: add custom user details service and user principal

---
+++ auth-service/src/main/java/com/d1ff/authservice/security/CustomUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.d1ff.authservice.security;
+
+import com.d1ff.authservice.entity.User;
+import com.d1ff.authservice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new UserPrincipal(user);
+    }
+}
---
+++ auth-service/src/main/java/com/d1ff/authservice/security/UserPrincipal.java
@@ -0,0 +1,33 @@
+package com.d1ff.authservice.security;
+
+import com.d1ff.authservice.entity.User;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class UserPrincipal implements UserDetails {
+
+    @Getter
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPasswordHash();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+}
